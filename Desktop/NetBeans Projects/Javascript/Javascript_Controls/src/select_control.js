1643610ff2166c62f2081497722200435dc1bda7
Ovidiu <enescu_vd@yahoo.com>
2017-09-16 15:18:03 +0300

    added select element control

---
+++ Desktop/NetBeans Projects/Javascript/Javascript_Controls/src/select_control.js
@@ -0,0 +1,94 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+var select = function (params) {
+    var self = this;
+    this.id = "select-" + new Date().getTime();
+    this.html = "<select id=" + this.id + "><option>Please select option</option></select>";
+    this.jQuery = "#" + this.id;
+
+    this.params = {
+        target: $('body'),
+        source: []
+    }
+
+    if (params)
+        $.extend(true, this.params, params);
+
+    this.getSelected = function () {
+        var option = $(self.jQuery).find("option:selected");
+        return {
+            id: option.prop('id'),
+            value: option.text().trim()
+        };
+    }
+
+    this.setValues = function (values) {
+        self.params.source = values;
+        self.removeValues();
+        values.forEach(function (v) {
+            var option = $("<option id=" + v.value + ">" + v.text + "</option>");
+            $(self.jQuery).append(option);
+        });
+    }
+
+    this.removeValues = function () {
+        $(self.jQuery).find('option').filter(function () {
+            return $(this).text().trim().toUpperCase() !== "PLEASE SELECT OPTION";
+        }).remove();
+    }
+
+    this.init = function () {
+        self.params.target.append($(self.html));
+        self.setValues(params.source);
+        self.defaultCSS();
+    }
+
+    this.defaultCSS = function () {
+        $(self.jQuery).css({
+            padding: '5px',
+            'border-radius': '5px'
+        });
+
+        /*$(self.jQuery).find('option').mouseenter(function() {
+         $(this).css({
+         background: 'lightgray',
+         color: 'blue'
+         })
+         }).mouseleave(function() {
+         $(this).css({
+         background: 'white',
+         color: 'black'
+         })
+         });*/
+    }
+
+    this.destroy = function () {
+        document.getElementById(self.id).remove();
+        self = null;
+    }
+}
+
+var params = {
+    source: [{
+            text: "Pizza",
+            value: "Pizza"
+        }, {
+            text: "Lasagna",
+            value: "Lasagna"
+        }, {
+            text: "Ciorba",
+            value: "Ciorba"
+        }, {
+            text: "French fries",
+            value: "French fries"
+        }, {
+            text: "Pasta",
+            value: "Pasta"
+        }],
+    target: $('body')
+};
